db11cbc6ef62b5920246041332ace03657b4e4a1
Kolawole Oluwagbemiga <kola2798@gmail.com>
2022-02-25 09:39:02 +0100

    Add files via upload

---
+++ Profiler.py
@@ -0,0 +1,24 @@
+from turtle import color
+
+
+name = input("Input your Name: ")
+person_age = input("Input your Age: ")
+home_address = input("Input your Home Adrress: ")
+gender = input("Input your Gender: ")
+email = input("Input your Email: ")
+phonenumber = input("Input your Phonenumber: ")
+facebook_handle = input("Input your Facebook Handle: ")
+twitter_handle = input("Input your Twitter Handle: ")
+instagram_handle = input("Input your Instagram Handle: ")
+tiktok_handle = input("Input your Tiktok Handle: ")
+current_occupation = input("Input your Current Occupation: ")
+country_of_origin = input("Input your Country of Origin: ")
+father_name = input("Input your Father Name: ")
+mother_name = input("Input your Mother Name: ")
+siblings_names = input("Input your Siblings Names: ")
+dob = input("Input your dob: ")
+favourite_color = input("Input your Favourite Color: ")
+favourite_food = input("Input your Favourite Food: ")
+relationship_status = input("Input your Relationship Status: ")
+bio_pic = '\n' + name + '\n' + person_age + '\n' + home_address + '\n' + gender + '\n' + email + '\n' + phonenumber + '\n' + facebook_handle + '\n' + twitter_handle + '\n' +  instagram_handle + '\n' + tiktok_handle + '\n' + current_occupation + '\n' + country_of_origin + '\n' + father_name + '\n' + mother_name + '\n' + siblings_names + '\n' + dob + '\n' + favourite_color + '\n' + favourite_food +'\n'+ relationship_status
+print('Your Bio Pic is ' + bio_pic)
